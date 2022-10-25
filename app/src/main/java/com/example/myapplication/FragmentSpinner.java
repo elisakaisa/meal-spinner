25ac76ac2b22c3486f7adfbb9ed2a81c19683a2d
elisakaisa <elisa.k.perini@gmail.com>
2022-10-25 17:47:24 +0200

    added delay so that each spinner spins one after the other

--- app/src/main/java/com/example/myapplication/FragmentSpinner.java
+++ app/src/main/java/com/example/myapplication/FragmentSpinner.java
@@ -4,27 +4,37 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.logic.SpinnerLogic;
-import com.example.myapplication.model.Meal;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentSpinner#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class FragmentSpinner extends Fragment {
 
     public FragmentSpinner() {
         // Required empty public constructor
     }
 
-    private SpinnerLogic spinnerLogic;
+    private SpinnerLogic spinnerLogic1, spinnerLogic2, spinnerLogic3;
+    MaterialButton btnSpin;
+    private boolean isStarted;
+    private Handler handler;
+    int i = 0;
+
+    private final List<String> proteinList = Arrays.asList("Fish", "Minced meat");
+    private final List<String> carbList = Arrays.asList("Pasta", "Rice", "Potatoes");
+    private final List<String> greenList = Arrays.asList("Cucumber", "Paprika", "Green peas");
 
     /*---------- HOOKS -----------*/
     MaterialTextView tvProtein, tvCarbs, tvGreens;
@@ -49,21 +59,77 @@ public class FragmentSpinner extends Fragment {
         tvProtein = view.findViewById(R.id.tv_spinner_protein);
         tvCarbs = view.findViewById(R.id.tv_spinner_carbs);
         tvGreens = view.findViewById(R.id.tv_spinner_greens);
-        MaterialButton btnSpin = view.findViewById(R.id.btn_spin);
+        btnSpin = view.findViewById(R.id.btn_spin);
 
-        /*-------- GAME LOGIC -------*/
-        spinnerLogic = SpinnerLogic.getInstance(); // singleton
+        /*---------------- START GAME ----------------------*/
+        handler = new Handler();
         
         /*-------- LISTENERS --------*/
-        btnSpin.setOnClickListener(v -> spin());
+        btnSpin.setOnClickListener(v -> spinner());
         
         return view;
     }
 
-    private void spin() {
-        Meal meal = spinnerLogic.returnRandomMeal();
-        tvProtein.setText(meal.getProtein());
-        tvCarbs.setText(meal.getCarb());
-        tvGreens.setText(meal.getGreen());
+    private void spin1() {
+        spinnerLogic1 = new SpinnerLogic(s -> requireActivity().runOnUiThread(() -> {
+            tvProtein.setText(s);
+        }), 200, randomLong(150, 400), proteinList);
+        spinnerLogic1.start();
     }
+
+    private void spin2() {
+        spinnerLogic1.stopWheel();
+        spinnerLogic2 = new SpinnerLogic(s -> requireActivity().runOnUiThread(() -> {
+            tvCarbs.setText(s);
+        }), 200, randomLong(150, 400), carbList);
+        spinnerLogic2.start();
+    }
+    private void spin3() {
+        spinnerLogic2.stopWheel();
+        spinnerLogic3 = new SpinnerLogic(s -> requireActivity().runOnUiThread(() -> {
+            tvGreens.setText(s);
+        }), 200, randomLong(150, 400), greenList);
+        spinnerLogic3.start();
+    }
+    private void stopSpinner() {
+        spinnerLogic3.stopWheel();
+    }
+
+
+    public static final Random RANDOM = new Random();
+
+    public static long randomLong(long lower, long upper) {
+        return lower + (long) (RANDOM.nextDouble() * (upper - lower));
+    }
+
+    private void spinner() {
+        // TODO: refactor this monstrosity
+        btnSpin.setEnabled(false);
+        int DELAY = 2000;
+        spin1();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // this code will be executed after 2 seconds
+                spin2();
+
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        spin3();
+
+                        new Timer().schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                stopSpinner();
+                                requireActivity().runOnUiThread(() -> btnSpin.setEnabled(true));
+                            }
+                        }, DELAY);
+                    }
+                }, DELAY);
+            }
+        }, DELAY);
+
+    }
+
 }
--- app/src/main/java/com/example/myapplication/logic/SpinnerLogic.java
+++ app/src/main/java/com/example/myapplication/logic/SpinnerLogic.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class SpinnerLogic {
+public class SpinnerLogic extends Thread {
     private final List<String> proteinList = Arrays.asList("Fish", "Minced meat");
     private final List<String> carbList = Arrays.asList("Pasta", "Rice", "Potatoes");
     private final List<String> greenList = Arrays.asList("Cucumber", "Paprika", "Green peas");
@@ -20,9 +20,59 @@ public class SpinnerLogic {
         return new Meal(protein, carb, green);
     }
 
-    private static SpinnerLogic spinnerLogic = null;
-    public static SpinnerLogic getInstance() {
-        if (spinnerLogic == null) spinnerLogic = new SpinnerLogic();
-        return spinnerLogic;
+    public interface WheelListener {
+        void newString(String s);
+    }
+
+    private WheelListener wheelListener;
+    public int currentIndex;
+    private long frameDuration;
+    private long startIn;
+    private boolean isStarted;
+    private List<String> list;
+
+    public SpinnerLogic(WheelListener wheelListener, long frameDuration, long startIn, List<String> list) {
+        this.wheelListener = wheelListener;
+        this.frameDuration = frameDuration;
+        this.startIn = startIn;
+        this.list = list;
+        currentIndex = 0;
+        isStarted = true;
+    }
+
+    public void next(List<String> list) {
+        currentIndex++;
+
+        if (currentIndex == list.size()) {
+            currentIndex = 0;
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(startIn);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while(isStarted) {
+            try {
+                Thread.sleep(frameDuration);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            next(list);
+
+            if (wheelListener != null) {
+                wheelListener.newString(list.get(currentIndex));
+            }
+        }
+    }
+
+    public void stopWheel() {
+        isStarted = false;
     }
 }
+
